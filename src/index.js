41e9226be80baf2943ebf3a206e232b37895af9d
texasky <texasssky@gmail.com>
2017-09-29 05:51:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let res = true;
+
+  str.split('').forEach(bracket => {
+    if(bracketsConfig.find((el) => {
+      return el[0] === bracket && el[1] !== bracket;
+    })) {
+      stack.push(bracket);
+    } else if(bracketsConfig.find((el) => {
+      return el[1] === bracket && el[0] !== bracket
+    })) {
+      let confPair = bracketsConfig.find((el) => {
+          return el[1] === bracket
+      })
+      if(stack.pop() !== confPair[0]) {
+        res = false;
+      }
+    } else if(bracketsConfig.find((el) => {
+        return el[0] === bracket && el[1] === bracket;
+      })) {
+        if(stack[stack.length - 1] === bracket) {
+          stack.pop();
+        } else {
+          stack.push(bracket);
+        }
+    }
+  })
+
+  if(stack.length !== 0){
+    return false;
+  }
+  return res;
 }
